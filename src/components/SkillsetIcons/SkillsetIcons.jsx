2242c7e3025f35c0d655c8cc8ede58fe43ca3347
marinn999 <marinacompel@icloud.com>
2024-12-14 20:41:11 +0100

    skillset icons moving

--- src/components/SkillsetIcons/SkillsetIcons.jsx
+++ src/components/SkillsetIcons/SkillsetIcons.jsx
@@ -1,43 +1,52 @@
+import { useState, useEffect } from "react";
 import s from "./SkillsetIcons.module.css";
 
 const SkillsetIcons = () => {
+  const [position, setPosition] = useState(0);
+  const speed = 1; // move 1px
+
+  const items = [
+    { id: 1, width: 85, height: 21, icon: "icon-5" },
+    { id: 2, width: 71, height: 29, icon: "icon-6" },
+    { id: 3, width: 28, height: 28, icon: "icon-7" },
+    { id: 4, width: 21, height: 21, icon: "icon-8" },
+    { id: 5, width: 154, height: 25, icon: "icon-9" },
+    { id: 6, width: 180, height: 140, icon: "icon-10" },
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPosition((prev) => prev - speed); // move el left -1px
+    }, 16); // every 16 ms
+    return () => clearInterval(interval);
+  }, []);
+
+  const itemWidth = 200;
+  const totalWidth = itemWidth * items.length;
+
+  // Скидаємо позицію, коли список прокручено повністю
+  if (Math.abs(position) >= totalWidth) {
+    setPosition(0);
+  }
+
   return (
-    <>
-      <section className={s.section}>
-        <ul className={s.list}>
-          <li className={s.item}>
-            <svg className={s.icon} width="85" height="21">
-              <use href="symbol-defs.svg#icon-5"></use>
+    <section className={s.section}>
+      <ul
+        className={s.list}
+        style={{
+          transform: `translateX(${position}px)`,
+        }}
+      >
+        {/* Дублюємо масив елементів для безшовного ефекту */}
+        {[...items, ...items].map((item, index) => (
+          <li key={index} className={s.item}>
+            <svg className={s.icon} width={item.width} height={item.height}>
+              <use href={`symbol-defs.svg#${item.icon}`} />
             </svg>
           </li>
-          <li className={s.item}>
-            <svg className={s.icon} width="71" height="29">
-              <use href="symbol-defs.svg#icon-6"></use>
-            </svg>
-          </li>
-          <li className={s.item}>
-            <svg className={s.icon} width="28" height="28">
-              <use href="symbol-defs.svg#icon-7"></use>
-            </svg>
-          </li>
-          <li className={s.item}>
-            <svg className={s.icon} width="21" height="21">
-              <use href="symbol-defs.svg#icon-8"></use>
-            </svg>
-          </li>
-          <li className={s.item}>
-            <svg className={s.icon} width="154" height="25">
-              <use href="symbol-defs.svg#icon-9"></use>
-            </svg>
-          </li>
-          <li className={s.item}>
-            <svg className={s.icon} width="180" height="140">
-              <use href="symbol-defs.svg#icon-10"></use>
-            </svg>
-          </li>
-        </ul>
-      </section>
-    </>
+        ))}
+      </ul>
+    </section>
   );
 };
 
