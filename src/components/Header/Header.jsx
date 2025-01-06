3a255221a4de3784edbd89990a6babc918dd2e40
marinn999 <marinacompel@icloud.com>
2025-01-06 18:30:10 +0100

    mob menu started

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,20 @@
+import { useState } from "react";
 import s from "./Header.module.css";
+import ReactModal from "react-modal";
 
 const Header = () => {
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  const openMobileMenu = () => {
+    setMobileMenu(true);
+  };
+  const closeMobileMenu = () => {
+    setMobileMenu(false);
+  };
+  const handleMenuClick = () => {
+    closeMobileMenu();
+  };
+
   return (
     <>
       <header className={s.header}>
@@ -29,14 +43,43 @@ const Header = () => {
               </li>
             </ul>
           </nav>
-          <button className={s.btn}>
+          <button className={s.btn} onClick={openMobileMenu}>
             <svg width="24" height="24" aria-label="open menu button">
               <use href="/symbol-defs.svg#icon-13"></use>
             </svg>
           </button>
+          <ReactModal
+            isOpen={mobileMenu}
+            onRequestClose={closeMobileMenu}
+            contentLabel="Menu"
+            className={s.menu}
+            overlayClassName={s.overlay}
+            closeTimeoutMS={300}
+          >
+            <div className={s.modalHeader}>
+              <button className={s.modalCloseBtn} onClick={closeMobileMenu}>
+                Close
+              </button>
+              <ul className={s.modalList}>
+                <li className={s.modalItem} onClick={handleMenuClick}>
+                  <a href="#section-experience">Experience</a>
+                </li>
+                <li className={s.modalItem} onClick={handleMenuClick}>
+                  <a href="#section-work">Work</a>
+                </li>
+                <li className={s.modalItem} onClick={handleMenuClick}>
+                  <a href="#section-photo">Photography</a>
+                </li>
+                <li className={s.modalItem} onClick={handleMenuClick}>
+                  <a href="#section-contact">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </ReactModal>
         </div>
       </header>
     </>
   );
 };
+
 export default Header;
